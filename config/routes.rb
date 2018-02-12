5f8a45e9bd9c4bdcb452ba209d632b7b53b14792
Jeremy Red <zoomdude111@hotmail.com>
2018-02-12 00:07:55 -0800

    Created the basic pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'basic_pages/home'
+
+  get 'basic_pages/about'
+
+  get 'basic_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
